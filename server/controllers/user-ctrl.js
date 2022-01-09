af0e05b74e911b3fbd045c379a03c538d96ddd3b
rich5k <richardanatsui@gmail.com>
2022-01-09 01:03:49 +0000

    added deleteUser function

--- server/controllers/user-ctrl.js
+++ server/controllers/user-ctrl.js
@@ -71,7 +71,23 @@ const updateUser=async(req,res)=>{
     })
 }
 
+const deleteUser= async(req,res)=>{
+    await User.findOneAndDelete({_id: req.params.id}, (err, user)=>{
+        if(err){
+            return res.status(404).json({success: false, error:err})
+        }
+
+        if(!user){
+            return res 
+                .status(404)
+                .json({success: false, error:'User not found'})
+        }
+        return res.status(200).json({success: true, data: user})
+    }).catch(err=>console.log(err))
+}
 
 module.exports={
-    createUser
+    createUser,
+    updateUser,
+    deleteUser
 }
