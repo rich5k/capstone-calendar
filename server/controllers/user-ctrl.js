eaf8a0b20fabb35a4c42e1aef839fd5c54904025
rich5k <richardanatsui@gmail.com>
2022-01-09 00:58:53 +0000

    added updateUser function

--- server/controllers/user-ctrl.js
+++ server/controllers/user-ctrl.js
@@ -17,7 +17,7 @@ const createUser=async(req,res)=>{
     }
     User.create(obj, (err,item)=>{
         if(err){
-            return res.status(400).json({
+            return res.status(404).json({
                 err,
                 message: 'User not created'
             })
@@ -28,6 +28,46 @@ const createUser=async(req,res)=>{
                 message: 'User created'
             })
         }
+    })
+}
+
+const updateUser=async(req,res)=>{
+    const body = req.body;
+
+    if(!body){
+        return res.status(400).json({
+            success: false,
+            error: 'You must provide a body to create'
+        })
+    }
+    
+    User.findOne({_id: req.params.id}, (err,user)=>{
+        if(err){
+            return res.status(404).json({
+                err,
+                message: 'User not found'
+            })
+        }
+        user.name= body.name;
+        user.email= body.email;
+        user.password=body.password;
+        user.phoneNum= body.phoneNum;
+
+        user
+            .save()
+            .then(()=>{
+                return res.status(200).json({
+                    success: true,
+                    id:user._id,
+                    message: 'User updated'
+                })
+            })
+            .catch(error=>{
+                return res.status(404).json({
+                    error,
+                    message: 'User not updated'
+                })
+            })
     })
 }
 
