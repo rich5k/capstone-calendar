3b5ad30e3b78f3450be11237496f5bbf0c73fdf8
rich5k <richardanatsui@gmail.com>
2022-01-10 02:10:34 +0000

    added getUserById function

--- server/controllers/user-ctrl.js
+++ server/controllers/user-ctrl.js
@@ -86,8 +86,24 @@ const deleteUser= async(req,res)=>{
     }).catch(err=>console.log(err))
 }
 
+const getUserById =async(req,res)=>{
+    await User.findOne({_id: req.params.id}, (err,user)=>{
+        if(err){
+            return res.status(404).json({success:false,error:err})
+        }
+
+        if(!user){
+            return res
+                .status(404)
+                .json({success:false, error: 'User not found'})
+        }
+        return res.status(200).json({success:true, data: user})
+    }).catch(err=> console.log(err))
+}
+
 module.exports={
     createUser,
     updateUser,
-    deleteUser
+    deleteUser,
+    getUserById
 }
